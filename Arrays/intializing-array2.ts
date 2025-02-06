e3521e7cc0556a6558f823fdafea411f88c6c2f5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-06 02:13:18 -0300

    added array2 ts

---
+++ Arrays/intializing-array2.ts
@@ -0,0 +1,15 @@
+// how to intialize
+
+let daysOfWeek2: string[] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+console.log(daysOfWeek2.length);
+
+// install typescript properlu and then commiting the tsconfig
