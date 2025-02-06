13958573803fb44d841ac604f0ba94e79b0002c9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-06 02:04:50 -0300

    first commit

---
+++ Arrays/initializing-array.ts
@@ -0,0 +1,15 @@
+// Creating and initializing arrays
+
+let daysOfWeek = new Array();
+daysOfWeek = new Array(7);
+daysOfWeek = new Array(
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+);
+
+// Not the best practice
