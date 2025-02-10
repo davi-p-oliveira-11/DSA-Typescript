9f87f77166582814e4d6489f063815074981e1ca
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-10 11:48:56 -0300

    added isEven

---
+++ src/Arrays/09-isEven.ts
@@ -0,0 +1,10 @@
+//
+
+function isEven(x: number): boolean {
+  console.log(x);
+  return x % 2 === 0 ? true : false;
+}
+
+let numbers3: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(numbers3);
