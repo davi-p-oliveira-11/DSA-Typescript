4b41971d1dd9da827afa0813c2f3eb0dd5ac95bf
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-10 11:01:46 -0300

    added methods reference

---
+++ src/Arrays/07-methods reference.ts
@@ -0,0 +1,18 @@
+/*
+
+- **concat**: Joins multiple arrays and returns a copy of the concatenated arrays.  
+- **every**: Iterates through all elements of the array, checking a specified condition (function) until `false` is returned.  
+- **filter**: Creates an array with all elements that evaluate to `true` according to the specified function.  
+- **forEach**: Executes a specific function on each element of the array.  
+- **join**: Joins all elements of the array into a string.  
+- **indexOf**: Searches the array for specific elements and returns their position.  
+- **lastIndexOf**: Returns the position of the last item in the array that matches the search criterion.  
+- **map**: Creates another array based on a function that contains the criterion/condition and returns the elements that match the criterion.  
+- **reverse**: Reverses the array so that the last item becomes the first and vice versa.  
+- **slice**: Returns a new array starting from the specified index.  
+- **some**: Iterates through all elements of the array, checking the desired condition (function) until `true` is returned.  
+- **sort**: Sorts the array in alphabetical order or according to the specified function.  
+- **toString**: Returns the array in string form.  
+- **valueOf**: Similar to the `toString` method and returns the array as a string.
+
+*/
