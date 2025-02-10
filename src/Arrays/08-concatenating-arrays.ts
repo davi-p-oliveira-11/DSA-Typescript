dfef3b11c607ce7ff11eee1dcdd90e8c4997f180
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-10 11:09:00 -0300

    added concat arrays

---
+++ src/Arrays/08-concatenating-arrays.ts
@@ -0,0 +1,6 @@
+const zero: number = 0;
+const positiveNumbers: number[] = [1, 2, 3];
+const negativeNumbers: number[] = [-3, -2, -1];
+let numbers2: number[] = negativeNumbers.concat(zero, positiveNumbers);
+
+console.log(numbers2);
